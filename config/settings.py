52a6bcb06bd9ffd4ac5b60353c9531d479410ddd
mirodil <ortikov.m97@gmail.com>
2022-12-06 15:32:27 +0500

    env config

--- config/settings.py
+++ config/settings.py
@@ -27,11 +27,10 @@ environs.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
-
-CORS_ALLOWED_ORIGINS = ['http://localhost:8080']
+ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
+CORS_ALLOWED_ORIGINS = [env('CORS_ALLOWED_ORIGINS')]
 
 # Application definition
 
